6601638d8466a2b8846c18212e5e2f5a89144b04
Figlarz <Figlarz@Figlarzs-MacBook-Pro.local>
2017-03-01 19:59:00 +0100

    task 11.6

---
+++ scripts.js
@@ -0,0 +1,24 @@
+$(function() {
+    var url = 'https://restcountries.eu/rest/v1/name/',
+        countriesList = $('#countries');
+
+    $('#search').click(searchCountries);
+
+    function searchCountries() {
+        var countryName = $('#country-name').val();
+        if (!countryName.length) countryName = 'Poland';
+        $.getJSON(url + countryName, showCountriesList);
+/*    $.ajax({
+        url: url + countryName,
+        method: 'GET',
+        success: showCountriesList
+    }); */
+    }
+
+    function showCountriesList(resp) {
+        countriesList.empty();
+        resp.forEach(function(item) {
+            $('<li>').text(item.name).appendTo(countriesList);
+        });
+    }
+});
